e90bf3b96fbcfade324e5ff85c28dac72f3a0a42
KhangNgo-guest <85405931+KhangNgo-guest@users.noreply.github.com>
2021-09-15 21:59:48 +0700

    update checkout

---
+++ Page/checkout-page/script.js
@@ -0,0 +1,5 @@
+var movie = document.getElementById("movie-title").value;
+var cinema = document.getElementById("cinema-address").value;
+var seatType = document.getElementById("type").value;
+var ticketType = document.getElementById("quantity").value;
+var total = document.getElementById("cost").value;
