58a781e89b748c4df251e2cce5b604149e72da00
KhaB-BIT <khabui.31191025985@st.ueh.edu.vn>
2021-09-19 21:50:23 +0700

    complete search page

---
+++ Page/search-page/sub-filter.php
@@ -0,0 +1,7 @@
+<div class="sub-filter d-flex align-items-center">
+    <h4 class="sub-filter__text">Sắp xếp theo:</h4>
+    <select class="sub-filter__form">
+        <option value="1">Phim đang chiếu</option>
+        <option value="2">Phim sắp chiếu</option>
+    </select>
+</div> 
---
+++ Page/search-page/sub-searchbox.php
@@ -0,0 +1,25 @@
+<!-- ok  -->
+<div class="select d-flex">
+
+    <div class="select__items d-flex">
+        <div class="select__items--wrapper d-flex justify-content-center align-items-center">
+            <i class="fas fa-map-marker-alt"></i>
+        </div>
+        <div class="d-flex align-items-center select__items--text">Thành phố</div>
+    </div>
+
+    <div class="select__items d-flex">
+            <div class="select__items--wrapper d-flex justify-content-center align-items-center">
+                <i class="fas fa-home"></i>
+            </div>
+        <div class="d-flex align-items-center select__items--text">Rạp chiếu</div>
+    </div>
+
+    <div class="select__items d-flex">
+        <div class="select__items--wrapper d-flex justify-content-center align-items-center">
+            <i class="far fa-calendar-alt"></i>
+        </div>
+        <div class="d-flex align-items-center select__items--text">Ngày chiếu</div>
+    </div>   
+              
+</div>
