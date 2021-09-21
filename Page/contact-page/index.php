daa11702d4473b4136914e84bfa7fd89ed2770e5
KhaB-BIT <khabui.31191025985@st.ueh.edu.vn>
2021-09-21 21:46:10 +0700

    create folder contact-page

---
+++ Page/contact-page/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <link rel="stylesheet" href="./index.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container-fluid">
+        <div class="row" style="padding-top:100px">
+            <div class="col-xl-1"></div>
+            <div class="col-12 col-lg-6 col-xl-5 form-wrapper">
+                <div class="mb-3">
+                    <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Nhập tên của bạn">
+                </div>
+                <div class="mb-3">
+                    <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Điện thoại">
+                </div>
+                <div class="mb-3">
+                    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Email">
+                </div>
+                <div class="mb-3">
+                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="5" placeholder="Bạn muốn nói gì với chúng tôi"></textarea>
+                </div>    
+                <div class="btn">
+                    <button class="btn-text">Gửi</button>
+                </div>
+            </div>
+            <div class="col-12 col-lg-6 col-xl-5">
+                    <img src="./Mail sent-rafiki.svg" alt="">
+            </div>
+        </div>
+    </div>
+</body>
+</html>
