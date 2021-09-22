8bc956ed959b5d5209ea878ea942d479bd62dc46
truckhang181001 <85405909+truckhang181001@users.noreply.github.com>
2021-09-22 21:44:01 +0700

    Delete Page directory

--- Page/checkout-page/script.js
+++
@@ -1,5 +0,0 @@
-var movie = document.getElementById("movie-title").value;
-var cinema = document.getElementById("cinema-address").value;
-var seatType = document.getElementById("type").value;
-var ticketType = document.getElementById("quantity").value;
-var total = document.getElementById("cost").value;
