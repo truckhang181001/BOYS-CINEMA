b841593077646f4be5a25030d641437c5a505bc5
TrungHuy1005 <trunghuy0501@gmail.com>
2021-09-25 01:56:47 +0700

    update div

--- App/Views/InfoPage/header.php
+++ App/Views/InfoPage/header.php
@@ -1,57 +1,55 @@
 <!-- HEADER -->
 <div class="info-page__header__banner">
-                <img src="/BOYSCINEMA/public/img/info-page-banner.png" alt="Banner">
+    <img src="/BOYSCINEMA/public/img/info-page-banner.png" alt="Banner">
+    <div class="info-page__header__poster-info">
+        <p class="info-page__header__poster-info--name info-page--effect">YOU CAN'T SAVE THE WORLD ALONE</p>
+        <button type="button" class="info-page__header__poster-info--brand col-6">
+            Hành động
+        </button>
+        <div class="info-page__header__poster-info--time">
+            <div><i class="far fa-calendar-alt"></i> 10 May, 2021</div>
+            <div><i class="far fa-clock "></i> 2 hrs 50 mins</div>
+        </div>                       
+    </div>
+</div>
+
+<div class="info-page__header__btn-group">
+    <img class="info-page--effect info-page__header__btn-group--img" src="/BOYSCINEMA/public/img/info-page-poster.png">
+    <div class="info-page--fas-style">
+        <i class="fas fa-pause info-page--fas-style__poster-icon"></i>
+    </div>
+    <div class="info-page--fas-style">
+        <i class="fas fa-play info-page--fas-style__poster-icon"></i>
+    </div>                                     
+    <div class="col-xl-8 col-12 info-page__header__btn-group__statistic">
+        <div class="info-page__header__btn-group__statistic__ticket-number">
+            <div><i class="fas fa-clipboard-list"></i> (1005)</div>
+            <p class="col-12">Số vé bán</p>
+        </div>
+        <div class="info-page__header__btn-group__statistic__total-post">
+            <div>
+                <b class="">(5.0)</b>
+                <i class="fas fa-star--style fa-star"></i>
+                <i class="fas fa-star--style fa-star"></i>
+                <i class="fas fa-star--style fa-star"></i>
+                <i class="fas fa-star--style fa-star"></i>
+                <i class="fas fa-star--style fa-star"></i>
             </div>
-            <div class="info-page__header__btn-group">
-                <div class="info-page__header__btn-group__poster row col-xl-4 col-sm-6 col-12">
-                    <img class="col-xl-6 col-0 info-page--effect" src="/BOYSCINEMA/public/img/info-page-poster.png">
-                    <div class="info-page--fas-style">
-                    <i class="fas fa-pause info-page--fas-style__poster-icon"></i>
-                    </div>
-                    <div class="info-page--fas-style">
-                    <i class="fas fa-play info-page--fas-style__poster-icon"></i>
-                    </div>                                     
-                    <div class="col-xl-8 col-md-12 col-11 info-page__header__btn-group__poster__info">
-                        <p class="info-page__header__btn-group__poster__info--name info-page--effect">YOU CAN'T SAVE THE WORLD ALONE</p>
-                        <button type="button" class="info-page__header__btn-group__poster__info--brand col-6">
-                            Hành động
-                        </button>
-                        <div class="info-page__header__btn-group__poster__info--time col-8 col-sm-10 col-md-12 col-xl-11">
-                            <div><i class="far fa-calendar-alt"></i> 10 May, 2021</div>
-                            <div><i class="far fa-clock "></i> 2 hrs 50 mins</div>
-                        </div>
-                       
-                    </div>
-                </div>
-                <div class="col-xl-8 col-12 info-page__header__btn-group__statistic">
-                    <div class="info-page__header__btn-group__statistic__ticket-number">
-                        <div><i class="fas fa-clipboard-list"></i> (1005)</div>
-                        <p class="col-12">Số vé bán</p>
-                    </div>
-                    <div class="info-page__header__btn-group__statistic__total-post">
-                        <div>
-                            <b class="">(5.0)</b>
-                            <i class="fas fa-star--style fa-star"></i>
-                            <i class="fas fa-star--style fa-star"></i>
-                            <i class="fas fa-star--style fa-star"></i>
-                            <i class="fas fa-star--style fa-star"></i>
-                            <i class="fas fa-star--style fa-star"></i>
-                        </div>
-                        <p>Đánh giá từ khách hàng</p>
-                    </div>
-                    <div class="info-page__header__btn-group__statistic__rate">
-                        <div>
-                            <i class="fas fa-star--style fa-star"></i>
-                            <i class="fas fa-star--style fa-star"></i>
-                            <i class="fas fa-star--style fa-star"></i>
-                            <i class="fas fa-star--style fa-star"></i>
-                            <i class="fas fa-star--style fa-star"></i>
-                        </div>
-                        <p>Chọn đánh giá</p>
-                    </div>
-                    <button type="button" class=" info-page__header__btn-group__statistic--btnbook info-page--effect">
-                        MUA VÉ
-                    </button>
-                </div>
-            </div>            
+            <p>Đánh giá từ khách hàng</p>
+        </div>
+        <div class="info-page__header__btn-group__statistic__rate">
+            <div>
+                <i class="fas fa-star--style fa-star"></i>
+                <i class="fas fa-star--style fa-star"></i>
+                <i class="fas fa-star--style fa-star"></i>
+                <i class="fas fa-star--style fa-star"></i>
+                <i class="fas fa-star--style fa-star"></i>
+            </div>
+            <p>Chọn đánh giá</p>
+        </div>
+        <button type="button" class=" info-page__header__btn-group__statistic--btnbook info-page--effect">
+            MUA VÉ
+        </button>
+     </div>
+</div>            
 <!-- HEADER -->
