56982839082a8bda060cca9780f2d85be6e19cbd
Truckhang181001 <Truckhang181001@gmail.com>
2021-09-24 16:48:43 +0700

    Fix form, input, a tag

--- App/Views/BookingPage/index.php
+++ App/Views/BookingPage/index.php
@@ -7,83 +7,81 @@
         <div class="booking-page__banner">
             <img src="<?php echo PRONAME?>/public/img/BuyTicket.jpg" alt="">
         </div>
-        <div class="booking-page__filter">
-            <div class="booking-page__filter__row --start">
-                <div class="city-sort filter-item">
-                    <div class="filter-item__icon">
-                        <i class="fas fa-map-marker-alt"></i>
+        <form method="get" class="booking-page__form">
+            <div class="booking-page__form__filter">
+                <div class="booking-page__form__filter__row --start">
+                    <div class="city-sort filter-item">
+                        <div class="filter-item__icon">
+                            <i class="fas fa-map-marker-alt"></i>
+                        </div>
+                        <select class="filter-item__form" name="city">
+                            <option selected>Chọn thành phố</option>
+                            <option value="1">TP.HCM</option>
+                            <option value="2">Hà Nội</option>
+                            <option value="3">Kiên Giang</option>
+                            <option value="2">Nha Trang</option>
+                        </select>
                     </div>
-                    <select class="filter-item__text form-select">
-                        <option selected>Thành phố</option>
-                        <option value="1">One</option>
-                        <option value="2">Two</option>
-                        <option value="3">Three</option>
-                    </select>
-                    <div class="filter-item__arr-icon">
-                        <i class="fas fa-chevron-down"></i>
+                    <div class="theater-sort filter-item">
+                        <div class="filter-item__icon">
+                            <i class="fas fa-film"></i>
+                        </div>
+                        <select class="filter-item__form" name="theater">
+                            <option selected>Chọn rạp</option>
+                            <option value="1">06 Sư Vạn Hạnh, Q10</option>
+                            <option value="2">256 Pasteur</option>
+                        </select>
                     </div>
                 </div>
-                <div class="theater-sort filter-item">
-                    <div class="filter-item__icon">
-                        <i class="fas fa-film"></i>
+                <div class="booking-page__form__filter__row --mid">
+                    <div class="theater-sort filter-item">
+                        <div class="filter-item__icon">
+                            <i class="fas fa-film"></i>
+                        </div>
+                        <select class="filter-item__form" name="film">
+                            <option selected>Chọn phim</option>
+                            <option value="1">Shang-chi cuộc chiến không hồi kết</option>
+                            <option value="2">Hai Phượng</option>
+                        </select>
                     </div>
-                    <div class="filter-item__text">
-                        Rạp
+                    <div class="method-sort filter-item">
+                        <div class="filter-item__icon">
+                            <i class="fas fa-vr-cardboard"></i>
+                        </div>
+                        <select class="filter-item__form" name="experience">
+                            <option selected>Hình thức</option>
+                            <option value="1">2D</option>
+                            <option value="2">3D</option>
+                        </select>
                     </div>
-                    <div class="filter-item__arr-icon">
-                        <i class="fas fa-chevron-down"></i>
+                </div>
+                <div class="booking-page__form__filter__row --end">
+                    <div class="date-sort filter-item">
+                        <div class="filter-item__icon">
+                            <i class="fas fa-film"></i>
+                        </div>
+                        <select class="filter-item__form" name="date">
+                            <option selected>Chọn ngày</option>
+                            <option value="1">21/10/2021</option>
+                            <option value="2">22/10/2021</option>
+                        </select>
+                    </div>
+                    <div class="time-sort filter-item">
+                        <div class="filter-item__icon">
+                            <i class="far fa-clock"></i>
+                        </div>
+                        <select class="filter-item__form" name="time">
+                            <option selected>Chọn suất chiếu</option>
+                            <option value="1">7:00</option>
+                            <option value="2">7:30</option>
+                        </select>
                     </div>
                 </div>
             </div>
-            <div class="booking-page__filter__row --mid">
-                <div class="theater-sort filter-item">
-                    <div class="filter-item__icon">
-                        <i class="fas fa-film"></i>
-                    </div>
-                    <div class="filter-item__text">
-                        Phim
-                    </div>
-                    <div class="filter-item__arr-icon">
-                        <i class="fas fa-chevron-down"></i>
-                    </div>
-                </div>
-                <div class="method-sort filter-item">
-                    <div class="filter-item__icon">
-                        <i class="fas fa-vr-cardboard"></i>
-                    </div>
-                    <div class="filter-item__text">
-                        Hình thức
-                    </div>
-                    <div class="filter-item__arr-icon">
-                        <i class="fas fa-chevron-down"></i>
-                    </div>
-                </div>
-            </div>
-            <div class="booking-page__filter__row --end">
-                <div class="date-sort filter-item">
-                    <div class="filter-item__icon">
-                        <i class="fas fa-film"></i>
-                    </div>
-                    <div class="filter-item__text">
-                        Ngày
-                    </div>
-                    <div class="filter-item__arr-icon">
-                        <i class="fas fa-chevron-down"></i>
-                    </div>
-                </div>
-                <div class="time-sort filter-item">
-                    <div class="filter-item__icon">
-                        <i class="far fa-clock"></i>
-                    </div>
-                    <div class="filter-item__text">
-                        Suất chiếu
-                    </div>
-                    <div class="filter-item__arr-icon">
-                        <i class="fas fa-chevron-down"></i>
-                    </div>
-                </div>
-            </div>    
-        </div>
+            <div class="booking-page__form__btn">
+                <input type="submit" class="btn" value="Tìm ghế">
+            </div>   
+        </form>
         <div class="booking-page__seat">
             <?php
                 $alphabet = range("A","Z");
--- App/Views/ContactPage/index.php
+++ App/Views/ContactPage/index.php
@@ -4,29 +4,29 @@
 </head>
 <body>
     <div class="container-fluid">
-        <div class="row" style="padding-top:100px">
+        <form class="row" style="padding-top:100px" method="post">
             <div class="col-md-1 col-lg-1"></div>
             <div class="col-12 col-md-6 col-lg-5 form-wrapper">
                 <div class="mb-3">
-                    <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Nhập tên của bạn">
+                    <input type="text" class="form-control" id="exampleFormControlInput1" name="name" placeholder="Nhập tên của bạn">
                 </div>
                 <div class="mb-3">
-                    <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Điện thoại">
+                    <input type="number" class="form-control" id="exampleFormControlInput1" name="phone" placeholder="Điện thoại">
                 </div>
                 <div class="mb-3">
-                    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Email">
+                    <input type="email" class="form-control" id="exampleFormControlInput1" name="email" placeholder="Email">
                 </div>
                 <div class="mb-3">
-                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="5" placeholder="Bạn muốn nói gì với chúng tôi"></textarea>
+                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="5" name="content" placeholder="Bạn muốn nói gì với chúng tôi"></textarea>
                 </div>    
                 <div class="button-send">
-                    <button class="button-send--text">Gửi</button>
+                    <input class="button-send--text btn" type="submit" value="Gửi"></input>
                 </div>
             </div>
             <div class="col-12 col-md-4 col-lg-5">
                     <img src="<?php echo PRONAME?>/public/img/Mail sent-rafiki.svg" alt="">
             </div>
-        </div>
+        </form>
     </div>
 </body>
 </html>
