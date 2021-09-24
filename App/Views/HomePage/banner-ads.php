8fc0f59b6fe130d7d929ed8eaf620e3a4aeb7dab
Phan Khải <khai1272001@outlook.com>
2021-09-24 17:27:04 +0700

    khai co gang

---
+++ App/Views/HomePage/banner-ads.php
@@ -0,0 +1,11 @@
+<div class="owl-carousel banner-ads owl-theme owl-banner">
+    <div class="banner-item">
+        <img src="/BOYSCINEMA/public/img/home-page-banner1.jpg" alt="Thumb" class="banner__img">
+    </div>
+    <div class="banner-item">
+        <img src="/BOYSCINEMA/public/img/home-page-banner2.jpg" alt="Thumb" class="banner__img">
+    </div>
+    <div class="banner-item">
+        <img src="/BOYSCINEMA/public/img/home-page-banner3.jpg" alt="Thumb" class="banner__img">
+    </div>
+</div>
--- public/js/HomePage/script.js
+++ public/js/HomePage/script.js
@@ -25,3 +25,56 @@ $(document).ready(function(){
         },
     })
 });
+
+    $('.banner-ads').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: false,
+        dots: false,
+        autoplay: true,
+        touchDrag: false,
+        mouseDrag: false,
+        pullDrag: false, 
+        autoplayTimeout:6000,
+        responsive: {
+            0:{
+                items:1,
+                nav:false
+            },
+            600:{
+                items:1,
+                nav:false
+            },
+            1000:{
+                items:1,
+                nav:false,
+            }
+        },
+    });
+
+    $('.event-ads').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        navText: [
+            `<i class="fas fa-chevron-left prev-btn"></i>`,
+            `<i class="fas fa-chevron-right next-btn"></i>`
+        ],
+        dots: false,
+        autoplay: true,
+        autoplayTimeout:3000,
+        responsive: {
+            0:{
+                items:1,
+                nav:true
+            },
+            600:{
+                items:1,
+                nav:true
+            },
+            1000:{
+                items:1,
+                nav:true,
+            }
+        },
+    });
