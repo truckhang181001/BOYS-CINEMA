303d5d78f3493cf8e8efb85765d0bc7bcc1c1b1b
KhaB-BIT <khabui.31191025985@st.ueh.edu.vn>
2021-11-26 23:09:52 +0700

    add update personal information

--- App/Controllers/personal_information.php
+++ App/Controllers/personal_information.php
@@ -2,7 +2,8 @@
     class personal_information extends Controller{
         function __construct()
         {
+            $tblCus = $this->GetModel("tbl_customer");
             $this->getView("personal_information_page");
-        }
+        }  
     }
 ?>
--- App/Views/personal_information_page/index.php
+++ App/Views/personal_information_page/index.php
@@ -1,4 +1,4 @@
-<form action="" method="$_POST">
+<form method="post">
     <section class="personal-info-section">
         <div class="personal-info">
             <div class="personal-info__sub-title">
@@ -13,56 +13,77 @@
                         <div class="personal-info__input__title">
                             HỌ VÀ TÊN<span>*</span>
                         </div>
-                        <input type="text" name="userPswConf" class="personal-info__input__username" style="color:white" value="Bùi Minh Kha" required>
+                        <input type="text" name="userName" class="personal-info__input__username" style="color:white" required>
                     </div>
                     <div class="personal-info__input mb-3">
                         <div class="personal-info__input__title">
                             EMAIL<span>*</span>
                         </div>
-                        <input type="email" name="userEmail" class="personal-info__input__email" style="color:white" value="buiminhkha@gmail.com" readonly>
+                        <input type="email" name="userEmail" class="personal-info__input__email" value="" style="color:white" readonly>
                     </div>
         
                     <div class="personal-info__input mb-3">
                         <div class="personal-info__input__title">
                             MẬT KHẨU<span>*</span>
                         </div>
-                        <input type="password" name="userPsw" class="personal-info__input__psw" value="12345678" style="color:white">
+                        <input type="password" name="userPsw" class="personal-info__input__psw" style="color:white">
                     </div>
         
                     <div class="personal-info__input mb-3">
                         <div class="personal-info__input__title">
                             GIỚI TÍNH<span>*</span>
                         </div>
-                        <select class="form-select" aria-label="Default select example">
-                            <option selected>Chọn giới tính</option>
-                            <option value="1">Nam</option>
-                            <option value="2">Nữ</option>
-                            <option value="3">Khác</option>
+                        <select  name="userSex" class="form-select" aria-label="Default select example">
+     
+                            <option value="Nam">Nam</option>
+                            <option value="Nữ">Nữ</option>
+                            <option value="Khác">Khác</option>
                         </select>
                     </div>
                 </div>
                 <div class="col-12 col-md-6">
                     <div class="personal-info__input mb-3">
                         <div class="personal-info__input__title">
-                            NĂM SINH<span>*</span>
+                            NGÀY SINH<span>*</span>
                         </div>
-                        <input style="color: white" type="date" name="userPswConf" class="personal-info__input__psw-confirm">
+                        <input style="color: white" type="date" name="userYear" class="personal-info__input__psw-confirm">
                     </div>
                     <div class="personal-info__input mb-3">
                         <div class="personal-info__input__title">
                             SỐ ĐIỆN THOẠI<span>*</span>
                         </div>
-                        <input style="color: white" type="number" name="userPswConf" class="personal-info__input__psw-confirm" value="0374996432" placeholder="Nhập số điện thoại">
+                        <input style="color: white" type="text" name="userPhone" class="personal-info__input__psw-confirm"  placeholder="Nhập số điện thoại">
                     </div>
                     <div class="personal-info__input mb-3">
                         <div class="personal-info__input__title">
                             ĐỊA CHỈ<span>*</span>
                         </div>
-                        <input style="color: white" type="text" name="userPswConf" class="personal-info__input__psw-confirm" value="768, Tân Kiều, Tháp Mười" placeholder="Nhập địa chỉ">
+                        <input style="color: white" type="text" name="userAddress" class="personal-info__input__psw-confirm"  placeholder="Nhập địa chỉ">
                     </div>
-                    <input type="submit" class="personal-info__btn btnf" value="CẬP NHẬT" style="border:none">
+                    <button class="personal-info__btn btnf" type="submit" name="updatePersonalInfo" value="1">Cập nhật</button>
                 </div>   
             </div>
         </div>
     </section>
 </form>
+
+<?php
+    $controller = new controller();
+    $tbl_customer = $controller->getModel("tbl_customer");
+    
+    if (isset($_POST['updatePersonalInfo'])) {
+        updateCustomer($tbl_customer);
+        header("Location: ".CURLINK);
+        exit;
+    }
+    function updateCustomer($tbl_customer)
+    {
+        $password =$_POST['userPsw'];
+        $name = $_POST['userName'];
+        $sex = $_POST['userSex'];
+        $dob = $_POST['userYear'];
+        $address =$_POST['userAddress'];
+        $phone = $_POST['userPhone'];
+        $tbl_customer->updateCustomer($_POST['updatePersonalInfo'], $password, $name, $sex, $dob, $address, $phone);
+    }
+?>
